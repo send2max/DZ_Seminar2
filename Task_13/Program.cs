add02cd06970b4efab0e1b9bd8ff50a48253189b
Max <send2max@bk.ru>
2022-08-11 16:49:44 +0300

    Решена третья задача

---
+++ Task_13/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Напишите программу, 
+//  которая выводит третью цифру заданного числа или 
+//  сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdSecond(int num)
+{
+    if (num > 99)
+    {
+        while (num > 1000)
+        {
+            num = num / 10;
+        }
+        return num % 10;
+    }
+    else return -1;
+
+}
+
+int result = thirdSecond(number);
+
+if (result !=-1)
+{
+    Console.WriteLine($"{number} -> {result}");
+
+}
+else Console.Write($"{number} ->третьей цифры нет"); 
+
+
+
+
+
+
+
---
+++ Task_15/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, 
+//  которая принимает на вход цифру, обозначающую день недели,
+//   и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Day_oof(int num)
+{
+if (number == 6 || number == 7)
+
+    Console.WriteLine($"{number}-> да");
+    
+
+else if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+
+    Console.WriteLine($"{number}-> нет");
+else 
+Console.WriteLine($"{number}-> неправильный запрос, введите число от 1 до 7");
+return number;
+
+}
+int result = Day_oof(number);
+
+// Я только не знаю, был ли смысл вообще все это в метод вставлять т к программа и без него работать будет?
+
